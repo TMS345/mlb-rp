65f2ed04258dbeec4ac9eed4357fe1756c8aff45
TMS345 <gershyman@gmail.com>
2022-07-07 02:29:50 +0000

    recent commit

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-URL = "https://httpbin.org/cookies"
+import http.client
+
+conn = http.client.HTTPSConnection("mlb-data.p.rapidapi.com")
+
+headers = {
+    'X-RapidAPI-Key': "SIGN-UP-FOR-KEY",
+    'X-RapidAPI-Host': "mlb-data.p.rapidapi.com"
+    }
+
+conn.request("GET", "/json/named.roster_40.bam?team_id='121'", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
