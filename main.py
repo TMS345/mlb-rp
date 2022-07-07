5b3cd7b0287188df2bd95b3eb040c81ac7ae3377
TMS345 <gershyman@gmail.com>
2022-07-07 03:30:36 +0000

    recent commit

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import http.client
 
 conn = http.client.HTTPSConnection("mlb-data.p.rapidapi.com")
 
-headers = {
+headers = 
+{
     'X-RapidAPI-Key': "SIGN-UP-FOR-KEY",
     'X-RapidAPI-Host': "mlb-data.p.rapidapi.com"
-    }
+}
 
 conn.request("GET", "/json/named.roster_40.bam?team_id='121'", headers=headers)
 
